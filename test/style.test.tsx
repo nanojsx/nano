0dd15eb6b47073d3681162561e8f26b853efed77
yandeu <20306025+yandeu@users.noreply.github.com>
2020-06-23 16:27:10 +0200

    Test styling

---
+++ test/style.test.tsx
@@ -0,0 +1,18 @@
+import Nano, { Component } from '../lib/index.js'
+import { wait } from './helpers.js'
+
+const spy = jest.spyOn(global.console, 'error')
+
+test('should render without errors', async (done) => {
+  class Test extends Component {
+    render() {
+      return <p style={{ color: 'red', fontSize: 22 }}>hello world</p>
+    }
+  }
+  const res = Nano.render(<Test />)
+
+  await wait()
+  expect(res.outerHTML).toBe('<p style="color:red;font-size:22;">hello world</p>')
+  expect(spy).not.toHaveBeenCalled()
+  done()
+})
