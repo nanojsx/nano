95e4327826df41600baec5f421704a1e9e8f34f4
yandeu <20306025+yandeu@users.noreply.github.com>
2020-06-23 17:45:47 +0200

    Testing with htm

---
+++ test/htm.test.ts
@@ -0,0 +1,20 @@
+import htm from 'htm'
+import Nano, { Component } from '../lib/index.js'
+import { wait, nodeToString } from './helpers.js'
+
+const spy = jest.spyOn(global.console, 'error')
+
+test('should render without errors', async (done) => {
+  const html = htm.bind(Nano.createElement)
+
+  const Root = () => {
+    return html`<div id="root"><h1>Hello</h1></div>`
+  }
+
+  const res = Nano.render(Root)
+
+  await wait()
+  expect(nodeToString(res)).toBe('<div id="root"><h1>Hello</h1></div>')
+  expect(spy).not.toHaveBeenCalled()
+  done()
+})
