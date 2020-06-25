e5cfcdd8ec3aced8464c94ae3f1990ff7a8c5b8e
Yannick <20306025+yandeu@users.noreply.github.com>
2020-06-25 15:14:42 +0200

    Added nestedRenderOutputs.test.tsx

---
+++ test/nestedRenderOutputs.test.tsx
@@ -0,0 +1,19 @@
+import Nano, { Component } from '../lib/index.js'
+import { wait, nodeToString } from './helpers.js'
+
+const spy = jest.spyOn(global.console, 'error')
+
+test('should render without errors', async (done) => {
+  const Hello = (props: any) => <p>Hello {props.name}</p>
+
+  const hello = Nano.render(<Hello name="John" />)
+
+  const Root = () => <div id="root">{hello}</div>
+
+  const root = Nano.render(<Root />)
+
+  await wait()
+  expect(nodeToString(root)).toBe('<div id="root"><p>Hello John</p></div>')
+  expect(spy).not.toHaveBeenCalled()
+  done()
+})
