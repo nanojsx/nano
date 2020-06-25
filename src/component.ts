df9101d3ea048cdadd4a3702bb50d3d5a4f520db
Yannick <20306025+yandeu@users.noreply.github.com>
2020-06-26 01:11:50 +0200

    Some component improvements

--- src/component.ts
+++ src/component.ts
@@ -2,27 +2,30 @@ export class Component {
   element: HTMLElement
   props: any
 
-  render(): HTMLElement | void {}
+  render(update?: any): HTMLElement | void {}
 
   /** Will forceRender the component */
-  update() {
-    if (!this.element) {
+  update(update?: any) {
+    // on fragments we choose the first child
+    const el = Array.isArray(this.element) ? this.element[0] : this.element
+
+    if (!el) {
       console.warn('"this.element" is not yet defined in update()')
       return
     }
 
     // get parent
-    const parent = this.element.parentElement
+    const parent = el.parentElement
     if (!parent) {
       console.warn('"this.element.parentElement" is not yet defined in update()')
       return
     }
 
     // remove component root
-    parent.removeChild(this.element)
+    parent.removeChild(el)
 
     // render component root
-    let element = this.render() as HTMLElement
+    let element = this.render(update) as HTMLElement
 
     // if it is a fragment, element will be an array
     if (Array.isArray(element)) element = element[0]
