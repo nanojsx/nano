ff5c673c9a2a458072c694ab774ae58bc99d02fe
Yannick <20306025+yandeu@users.noreply.github.com>
2020-06-26 00:05:55 +0200

    Update fragment.test.tsx

--- test/fragment.test.tsx
+++ test/fragment.test.tsx
@@ -17,3 +17,66 @@ test('should render without errors', async (done) => {
   expect(spy).not.toHaveBeenCalled()
   done()
 })
+
+test('should render without errors', async (done) => {
+  const Test = () => (
+    <Fragment>
+      <p>test</p>
+    </Fragment>
+  )
+  const Root = () => (
+    <div>
+      <Test />
+    </div>
+  )
+  const res = Nano.render(<Root />)
+
+  await wait()
+  expect(res.outerHTML).toBe('<div><p>test</p></div>')
+  expect(spy).not.toHaveBeenCalled()
+  done()
+})
+
+test('should render without errors', async (done) => {
+  const Test = () => (
+    <Fragment>
+      <p>line one</p>
+      <p>line two</p>
+    </Fragment>
+  )
+  const Root = () => (
+    <div>
+      <Test />
+    </div>
+  )
+  const res = Nano.render(<Root />)
+
+  await wait()
+  expect(res.outerHTML).toBe('<div><p>line one</p><p>line two</p></div>')
+  expect(spy).not.toHaveBeenCalled()
+  done()
+})
+
+test('should render without errors', async (done) => {
+  const Name = (props: any) => {
+    return <p>{props.name}</p>
+  }
+
+  const Test = () => (
+    <Fragment>
+      <p>John</p>
+      <Name name="Suzanne" />
+    </Fragment>
+  )
+  const Root = () => (
+    <div>
+      <Test />
+    </div>
+  )
+  const res = Nano.render(<Root />)
+
+  await wait()
+  expect(res.outerHTML).toBe('<div><p>John</p><p>Suzanne</p></div>')
+  expect(spy).not.toHaveBeenCalled()
+  done()
+})
