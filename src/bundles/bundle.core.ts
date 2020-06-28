9599b01a95e554a84cc218406c3a70a07a8243e3
Yannick <20306025+yandeu@users.noreply.github.com>
2020-06-28 23:17:59 +0200

    Added more exports to core bundle

--- src/bundles/bundle.core.ts
+++ src/bundles/bundle.core.ts
@@ -1,7 +1,8 @@
-import { render } from '../core'
+import { render, createElement } from '../core'
+import { nodeToString } from '../helpers'
 import { Component } from '../component'
 
 export default {
-  Nano: { render },
+  Nano: { render, createElement, nodeToString },
   Component,
 }
