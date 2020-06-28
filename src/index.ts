b9052776681980b0e16a312c41788d4998f62a07
Yannick <20306025+yandeu@users.noreply.github.com>
2020-06-28 18:17:20 +0200

    Export Img and Visible

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ import './types'
 import { createElement, render, removeAllChildNodes } from './core'
 import { Fragment, createContext, nodeToString } from './helpers'
 import { Component } from './component'
+import { Img } from './components/img'
 import { Link } from './components/link'
+import { Visible } from './components/visible'
 
 export default { createContext, createElement, render, removeAllChildNodes, nodeToString }
-export { Fragment, Component, Link }
+export { Fragment, Component, Img, Link, Visible }
