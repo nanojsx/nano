bc598ebfb7d978404630afba1d37e09d050c2412
yandeu <20306025+yandeu@users.noreply.github.com>
2020-07-18 22:54:04 +0200

    Added onNodeRemove

--- src/helpers.ts
+++ src/helpers.ts
@@ -3,3 +3,28 @@ export const nodeToString = (node: any) => {
   tmpNode.appendChild(node.cloneNode(true))
   return tmpNode.innerHTML
 }
+
+const isDescendant: any = (desc: any, root: any) => {
+  return !!desc && (desc === root || isDescendant(desc.parentNode, root))
+}
+
+// https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver
+export const onNodeRemove = (element: any, callback: any) => {
+  var observer = new MutationObserver((mutationsList) => {
+    mutationsList.forEach((mutation) => {
+      mutation.removedNodes.forEach((removed) => {
+        if (isDescendant(element, removed)) {
+          callback()
+          // allow garbage collection
+          observer.disconnect()
+          // @ts-ignore
+          observer = undefined
+        }
+      })
+    })
+  })
+  observer.observe(document, {
+    childList: true,
+    subtree: true,
+  })
+}
