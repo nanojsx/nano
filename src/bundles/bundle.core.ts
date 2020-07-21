61f2bd43fcd2142ac5dba675613eb98b20c601c5
Yannick <20306025+yandeu@users.noreply.github.com>
2020-07-21 16:42:46 +0200

    Remove nodeToString and Component from core

--- src/bundles/bundle.core.ts
+++ src/bundles/bundle.core.ts
@@ -1,9 +1,5 @@
 import { render, h } from '../core'
-import { nodeToString } from '../helpers'
-import { Component } from '../component'
 
 export default {
-  Nano: { render, h, nodeToString },
-  Component,
-  h,
+  Nano: { render, h },
 }
