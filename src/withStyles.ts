e7046c2f62d3b5fd4e9fd3a487bdece8425737b4
Yannick <20306025+yandeu@users.noreply.github.com>
2020-08-09 20:53:56 +0200

    Convert withStyles to TS

---
+++ src/withStyles.ts
@@ -0,0 +1,36 @@
+import { h } from './core'
+import { Component } from './component'
+import { Fragment } from './fragment'
+import { Helmet } from './components/helmet'
+
+export const withStyles: any = (styles: any) => (WrappedComponent: any) => {
+  return class extends Component {
+    render() {
+      const { children, ...rest } = this.props
+
+      const helmet = h(Helmet, null, h('style', null, styles))
+
+      const component =
+        children && children.length > 0
+          ? h(WrappedComponent, { ...rest }, children)
+          : h(WrappedComponent, { ...this.props })
+
+      return h(Fragment, null, helmet, component)
+
+      // same in JSX
+      // return (
+      //   <Fragment>
+      //     <Helmet>
+      //       <style>{styles}</style>
+      //     </Helmet>
+
+      //     {children && children.length > 0 ? (
+      //       <WrappedComponent {...rest}>{children}</WrappedComponent>
+      //     ) : (
+      //       <WrappedComponent {...this.props} />
+      //     )}
+      //   </Fragment>
+      // )
+    }
+  }
+}
