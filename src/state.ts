8323868e19b2eacf4abfd7a7d9cfb1ce0b3eab81
yandeu <20306025+yandeu@users.noreply.github.com>
2020-09-12 23:53:51 +0200

    Added _clearState()

--- src/state.ts
+++ src/state.ts
@@ -1,1 +1,7 @@
+/** Holds the state of the whole application. */
 export const _state: Map<string, any> = new Map()
+
+/** Clears the state of the whole application. */
+export const _clearState = () => {
+  _state.clear()
+}
