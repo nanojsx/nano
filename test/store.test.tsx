de6c943150fb6c0c96eaae02f86a359175c2a3c4
yandeu <20306025+yandeu@users.noreply.github.com>
2020-12-29 22:48:52 +0100

    Improved store tests

--- test/store.test.tsx
+++ test/store.test.tsx
@@ -52,3 +52,73 @@ test('should render without errors', async (done) => {
   expect(spy).not.toHaveBeenCalled()
   done()
 })
+
+test('should render without errors', async (done) => {
+  const someStore = new Store({ data: 'init' }, 'someStore', 'local')
+
+  expect(localStorage.getItem('someStore')).toBe('{"data":"init"}')
+
+  let didCancel = false
+  let newState = ''
+  let prevState = ''
+
+  class Child extends Component {
+    store = someStore.use()
+
+    didMount() {
+      this.store.subscribe((_newState: any, _prevState: any) => {
+        newState = _newState.data
+        prevState = _prevState.data
+        this.update()
+      })
+    }
+
+    didUnmount() {
+      this.store.cancel()
+      didCancel = true
+    }
+
+    render() {
+      return <div>This is the Child. Data: {this.store.state.data}</div>
+    }
+  }
+
+  class App extends Component {
+    willMount() {
+      this.state = { renderChild: true }
+
+      setTimeout(() => {
+        someStore.setState({ data: 'modified' })
+      }, 150)
+
+      setTimeout(() => {
+        this.setState({ renderChild: false }, true)
+      }, 250)
+    }
+
+    render() {
+      return <div>{this.state.renderChild ? <Child /> : <span>No Child Rendered.</span>}</div>
+    }
+  }
+
+  const html = Nano.render(<App />, document.body)
+
+  await wait(100)
+  expect(nodeToString(html)).toBe('<body><div><div>This is the Child. Data: init</div></div></body>')
+
+  await wait(100)
+  expect(nodeToString(html)).toBe('<body><div><div>This is the Child. Data: modified</div></div></body>')
+
+  await wait(100)
+  expect(nodeToString(html)).toBe('<body><div><span>No Child Rendered.</span></div></body>')
+
+  someStore.clear()
+  expect(localStorage.getItem('someStore')).toBeNull()
+
+  expect(didCancel).toBeTruthy()
+  expect(newState).toBe('modified')
+  expect(prevState).toBe('init')
+
+  expect(spy).not.toHaveBeenCalled()
+  done()
+})
