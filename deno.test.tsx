d7fa0f31fd66d4246fcd1175f27832a6c56acbbd
Yannick <20306025+yandeu@users.noreply.github.com>
2021-01-14 23:38:20 +0100

    Added testing for deno

---
+++ deno.test.tsx
@@ -0,0 +1,49 @@
+// @deno-types="./typings/global.d.ts"
+import { h, Helmet, renderSSR, Component } from './deno_lib/mod.ts'
+import { assertEquals } from 'https://deno.land/std@0.83.0/testing/asserts.ts'
+
+Deno.test('should render without errors', () => {
+  const comments = ['Comment One', 'Comment Two']
+
+  class Comments extends Component {
+    props: { comments: string[] }
+
+    render() {
+      return (
+        <ul>
+          {this.props.comments.map((comment: any) => {
+            return <li>{comment}</li>
+          })}
+        </ul>
+      )
+    }
+  }
+
+  const App = () => (
+    <div>
+      <Helmet>
+        <title>Nano JSX SSR</title>
+        <meta name="description" content="Nano JSX App" />
+      </Helmet>
+
+      <Helmet footer>
+        <script src="/bundle.js"></script>
+      </Helmet>
+
+      <h2>Comments</h2>
+      <div id="comments">
+        <Comments comments={comments} />
+      </div>
+    </div>
+  )
+
+  const ssr = renderSSR(<App />)
+  const { body, head, footer } = Helmet.SSR(ssr)
+
+  assertEquals(
+    body,
+    '<div><h2>Comments</h2><div id="comments"><ul><li>Comment One</li><li>Comment Two</li></ul></div></div>'
+  )
+  assertEquals(head.join('\n'), '<title>Nano JSX SSR</title><meta content="Nano JSX App" name="description" />')
+  assertEquals(footer.join('\n'), '<script src="/bundle.js"></script>')
+})
