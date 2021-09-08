de531615de38aec5f6881683021f1254f50480a8
shinyaigeek <philispaxil@gmail.com>
2021-09-08 16:16:44 +0900

    Feature: exclude children props from props

--- src/jsx-runtime/index.ts
+++ src/jsx-runtime/index.ts
@@ -1,13 +1,11 @@
 export { Fragment } from '../fragment'
 import { h } from '../core'
 
-const createNode: (type: any, props: any, key: string, source?: string, self?: string) => any = function (
-  type,
-  props
-) {
-    return h(type, props, props?.children ?? []);
+const createNode: (type: any, props: any, key: string, source?: string, self?: string) => any = function (type, props) {
+  const { children, ..._props } = props
+  return h(type, _props, children ?? [])
 }
 
-export { createNode as jsx };
-export { createNode as jsxs };
-export { createNode as jsxDev };
+export { createNode as jsx }
+export { createNode as jsxs }
+export { createNode as jsxDev }
