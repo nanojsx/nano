526f2da531385c395bbe3ad4736307ee276572e4
Shinyaigeek <Philispaxil@gmail.com>
2021-10-17 00:14:28 +0900

    Feature(core): add mode arguments for customElementsMode

--- src/customElementsMode.ts
+++ src/customElementsMode.ts
@@ -1,20 +1,18 @@
 import { render } from './core'
 
-export const defineAsCustomElements: (component: any, componentName: string) => void = function (
-  component,
-  componentName
-) {
-  customElements.define(
-    componentName,
-    class extends HTMLElement {
-      constructor() {
-        super()
-        const shadowRoot = this.attachShadow({ mode: 'closed' })
-        // because nano-jsx update need parentElement, so DocumentFragment is not usable...
-        const fragment = document.createElement('div')
-        render(component, fragment)
-        shadowRoot.append(fragment)
+export const defineAsCustomElements: (component: any, componentName: string, mode?: 'open' | 'closed') => void =
+  function (component, componentName, mode = 'closed') {
+    customElements.define(
+      componentName,
+      class extends HTMLElement {
+        constructor() {
+          super()
+          const shadowRoot = this.attachShadow({ mode })
+          // because nano-jsx update need parentElement, so DocumentFragment is not usable...
+          const fragment = document.createElement('div')
+          render(component, fragment)
+          shadowRoot.append(fragment)
+        }
       }
-    }
-  )
-}
+    )
+  }
