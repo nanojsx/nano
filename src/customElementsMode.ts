2661f997afb485245c1b514a14bbc03d76b9cd1f
Shinyaigeek <Philispaxil@gmail.com>
2021-10-17 02:07:53 +0900

    Feature(core): allow user to make customElements with props

--- src/customElementsMode.ts
+++ src/customElementsMode.ts
@@ -5,13 +5,24 @@ export const defineAsCustomElements: (component: any, componentName: string, mod
     customElements.define(
       componentName,
       class extends HTMLElement {
+        component: any
         constructor() {
           super()
           const shadowRoot = this.attachShadow({ mode })
           // because nano-jsx update need parentElement, so DocumentFragment is not usable...
           const fragment = document.createElement('div')
-          render(component, fragment)
+          this.component = component
+          render(this.component, fragment)
           shadowRoot.append(fragment)
+        }
+
+        static get observedAttributes() {
+          return Object.keys(component.props)
+        }
+
+        attributeChangedCallback(name: string, _: any, newValue: any) {
+          this.component.updatePropsValue(name, newValue)
+          this.component.update()
         }
       }
     )
