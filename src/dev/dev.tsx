96a89ecae3c9546fbf007ccc9ac93ce8097ced1b
Yannick <20306025+yandeu@users.noreply.github.com>
2021-10-25 21:11:35 +0200

    Reset dev.tsx

--- src/dev/dev.tsx
+++ src/dev/dev.tsx
@@ -1,45 +1,10 @@
 import * as Nano from '../core'
 import { Component } from '../component'
-import { defineAsCustomElements } from '../customElementsMode'
 
 class App extends Component {
-  styles = {
-    margin: '2em',
-    padding: '1em',
-    background: 'antiquewhite'
-  }
-
-  constructor(props: any) {
-    super(props)
-    this.props = { ...this.props, counter: 0 }
-    console.log('constructor')
-  }
-
-  willMount() {
-    console.log('will Mount')
-  }
-
-  didUnmount() {
-    console.log('did Unmount')
-  }
-  didMount() {
-    console.log('did Mount')
-  }
-
   render() {
-    console.log('render')
-    return (
-      <div
-        onClick={() => {
-          this.props.counter++
-          this.update()
-        }}
-        style={{ ...this.styles }}>
-        Click me {this.props.counter}
-        <b> {this.props.children}</b>
-      </div>
-    )
+    return <div>Nano JSX App</div>
   }
 }
 
-defineAsCustomElements(App, 'first-app', ['counter', 'children'])
+Nano.render(<App />, document.getElementById('root'))
