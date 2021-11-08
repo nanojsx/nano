e7dadb846db3c17cec34e0e23d32253f03ace188
yandeu <20306025+yandeu@users.noreply.github.com>
2021-11-08 18:52:35 +0100

    dev

--- src/dev/dev.tsx
+++ src/dev/dev.tsx
@@ -5,26 +5,61 @@ import * as Router from '../components/router'
 const pause = (ms: number = 1000): Promise<void> => new Promise(resolve => setTimeout(() => resolve(), ms))
 
 const fetchPostMock = async (id: string) => {
-  console.log(`fetch request to: /api/post/${id}`)
-  await pause(250)
+  await pause(Math.random() * 200 + 100)
+  console.log(`[Fetch] /api/post/${id}`)
   return `this is the content of post #${id}`
 }
 
 const Container: Nano.FC<{ children: any }> = ({ children }) => <div style="padding: 2em;">{children}</div>
 
 const Navigation = () => (
-  <div style="display: flex; flex-direction: column;">
+  <div style="display: flex; flex-wrap: wrap;">
+    <style>
+      {`a {
+        margin-right: 18px;
+        margin-bottom: 8px;
+        text-decoration: none;
+        color: blue;
+      }`}
+    </style>
     <Router.Link to="/">Home</Router.Link>
     <Router.Link to="/about">About</Router.Link>
     <Router.Link to="/post">All Posts</Router.Link>
     <Router.Link to="/post/1">Post 1</Router.Link>
     <Router.Link to="/post/2">Post 2</Router.Link>
-    <Router.Link to="/post/85">Post 85</Router.Link>
+    <Router.Link to="/post/3">Post 3</Router.Link>
   </div>
 )
 
+class Posts extends Component {
+  constructor(props: any) {
+    super(props)
+    console.log('[Posts] constructor')
+  }
+
+  async didMount() {
+    const posts = await Promise.all([fetchPostMock('1'), fetchPostMock('2'), fetchPostMock('3')])
+    this.update(posts)
+  }
+
+  render(posts = ['loading...']) {
+    return (
+      <div>
+        {posts.map((p: any) => (
+          <div>{p}</div>
+        ))}
+      </div>
+    )
+  }
+}
+
 class Post extends Component {
   route = Router.Listener.use()
+
+  constructor(props: any) {
+    super(props)
+    console.log('[Post] constructor')
+  }
 
   async didMount() {
     this.route.subscribe(async (currPath: string, prevPath: string) => {
@@ -32,7 +67,7 @@ class Post extends Component {
 
       // console.log('------')
       // const match = Router.matchPath(currPath, { path: this.props.route.path })
-      console.log(`Just navigated from ${prevPath} to ${currPath}`)
+      // console.log(`Just navigated from ${prevPath} to ${currPath}`)
       // console.log(this.props.route)
       // console.log(match)
       // console.log('------')
@@ -49,26 +84,34 @@ class Post extends Component {
   async loadPost(currentPath = window.location.pathname) {
     const match = Router.matchPath(currentPath, { path: this.props.route.path })
 
-    // fetch post by id
-    const post = await fetchPostMock(match?.params?.id)
-
     if (match !== null) {
+      const post = await fetchPostMock(match?.params?.id)
       this.update(post)
     }
   }
 
   render(content = 'loading...') {
-    return <div style="padding: 16px;">{content}</div>
+    return <div>{content}</div>
   }
 }
 
 const Routes = () => (
-  <Router.Switch fallback={() => <div>404 (not found)</div>}>
-    <Router.Route exact path="/"></Router.Route>
-    <Router.Route path="/post/:id">
-      <Post />
-    </Router.Route>
-  </Router.Switch>
+  <div id="content" style="margin-top: 16px;">
+    <Router.Switch fallback={() => <div>404 (not found)</div>}>
+      <Router.Route exact path="/">
+        <div>Welcome to my Homepage.</div>
+      </Router.Route>
+      <Router.Route exact path="/about">
+        /About
+      </Router.Route>
+      <Router.Route exact path="/post">
+        <Posts />
+      </Router.Route>
+      <Router.Route path="/post/:id">
+        <Post />
+      </Router.Route>
+    </Router.Switch>
+  </div>
 )
 
 class App extends Component {
