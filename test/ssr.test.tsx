a9ef02690872f28dd1fdfe1400e352aca9f7d21e
yandeu <20306025+yandeu@users.noreply.github.com>
2021-11-18 00:49:19 +0100

    Addition to ssr test

--- test/ssr.test.tsx
+++ test/ssr.test.tsx
@@ -7,114 +7,134 @@ import { initSSR, HTMLElementSSR, renderSSR } from '../lib/ssr.js'
 
 const spy = jest.spyOn(global.console, 'error')
 
-test('should render without errors', () => {
-  initSSR()
+describe('server side rendering', () => {
+  test('should render without errors', () => {
+    initSSR()
 
-  // @ts-ignore
-  const div = document.createElement('div') as HTMLElementSSR
-  expect(div.ssr).toBe('<div></div>')
+    // @ts-ignore
+    const div = document.createElement('div') as HTMLElementSSR
+    expect(div.ssr).toBe('<div></div>')
 
-  div.setAttributeNS('id', 'root')
-  expect(div.ssr).toBe('<div id="root"></div>')
+    div.setAttributeNS('id', 'root')
+    expect(div.ssr).toBe('<div id="root"></div>')
 
-  const ul = new HTMLElementSSR('ul')
-  const li_0 = new HTMLElementSSR('li')
-  const li_1 = new HTMLElementSSR('li')
-  li_0.innerText = 'one'
-  li_1.innerText = 'two'
-  ul.appendChild(li_0)
-  ul.appendChild(li_1)
-  div.appendChild(ul)
-  expect(div.ssr).toBe('<div id="root"><ul><li>one</li><li>two</li></ul></div>')
+    const ul = new HTMLElementSSR('ul')
+    const li_0 = new HTMLElementSSR('li')
+    const li_1 = new HTMLElementSSR('li')
+    li_0.innerText = 'one'
+    li_1.innerText = 'two'
+    ul.appendChild(li_0)
+    ul.appendChild(li_1)
+    div.appendChild(ul)
+    expect(div.ssr).toBe('<div id="root"><ul><li>one</li><li>two</li></ul></div>')
 
-  const children = ul.children
-  expect(children[0]).toBe('<li>one</li>')
-  expect(children[1]).toBe('<li>two</li>')
-  //@ts-ignore
-  ul.addEventListener('click', () => {})
+    const children = ul.children
+    expect(children[0]).toBe('<li>one</li>')
+    expect(children[1]).toBe('<li>two</li>')
+    //@ts-ignore
+    ul.addEventListener('click', () => {})
 
-  const newChild = new HTMLElementSSR('span')
-  newChild.innerText = 'hello'
-  div.replaceChild(newChild)
-  expect(div.ssr).toBe('<div id="root"><span>hello</span></div>')
+    const newChild = new HTMLElementSSR('span')
+    newChild.innerText = 'hello'
+    div.replaceChild(newChild)
+    expect(div.ssr).toBe('<div id="root"><span>hello</span></div>')
 
-  // others
-  expect(div.innerText).toBe('<span>hello</span>')
-  expect(document.querySelector('#id')).toBeUndefined() // querySelector always return undefined in SSR
+    // others
+    expect(div.innerText).toBe('<span>hello</span>')
+    expect(document.querySelector('#id')).toBeUndefined() // querySelector always return undefined in SSR
 
-  // @ts-ignore
-  expect(document.createElementNS('URI', 'div').ssr).toBe('<div></div>')
+    // @ts-ignore
+    expect(document.createElementNS('URI', 'div').ssr).toBe('<div></div>')
+  })
+
+  test('should render without errors', async () => {
+    const App = () => {
+      return (
+        <div>
+          <Helmet>
+            <title>some title</title>
+            <meta name="description" content="Nano-JSX application" />
+          </Helmet>
+          <Img src="some-url" placeholder="placeholder-url" />
+        </div>
+      )
+    }
+
+    const app = Nano.renderSSR(<App />)
+    const { body, head } = Helmet.SSR(app)
+
+    const html = `
+       <!DOCTYPE html>
+       <html lang="en">
+         <head>
+           ${head.join('\n')}
+         </head>
+         <body>
+           ${body}
+         </body>
+       </html>
+       `
+
+    expect(html).toBe(`
+       <!DOCTYPE html>
+       <html lang="en">
+         <head>
+           <title>some title</title><meta content="Nano-JSX application" name="description" />
+         </head>
+         <body>
+           <div><img src="placeholder-url" /></div>
+         </body>
+       </html>
+       `)
+    expect(spy).not.toHaveBeenCalled()
+  })
+
+  test("should escape attribute's string value", () => {
+    const content = Nano.h('div', { id: '"hoge' }, 'hoge')
+    const html = renderSSR(content)
+    expect(html).toBe('<div id="&quot;hoge">hoge</div>')
+  })
+
+  test('should escape dangerous string', () => {
+    const h1 = Nano.h('h1', {}, 'title')
+    const p = Nano.h('p', {}, 'yanni<k')
+    const div = Nano.h('div', {}, h1, p)
+    const html = renderSSR(div)
+    expect(html).toBe('<div><h1>title</h1><p>yanni&lt;k</p></div>')
+  })
 })
 
-test('should render without errors', async () => {
-  const App = () => {
-    return (
-      <div>
-        <Helmet>
-          <title>some title</title>
-          <meta name="description" content="Nano-JSX application" />
-        </Helmet>
-        <Img src="some-url" placeholder="placeholder-url" />
-      </div>
-    )
-  }
+describe('escape dangerous html', () => {
+  test('should keep dangerous string', () => {
+    const h1 = Nano.h('h1', {}, 'title')
+    const p = Nano.h('p', { dangerouslySetInnerHTML: { __html: 'des<ription' } })
+    const div = Nano.h('div', {}, h1, p)
+    const html = renderSSR(div)
+    expect(html).toBe('<div><h1>title</h1><p>des<ription</p></div>')
+  })
 
-  const app = Nano.renderSSR(<App />)
-  const { body, head } = Helmet.SSR(app)
+  test('should escape text node', () => {
+    const content = Nano.h('div', {}, '<span>span</span>')
+    const html = renderSSR(content)
+    expect(html).toBe('<div>&lt;span&gt;span&lt;/span&gt;</div>')
+  })
 
-  const html = `
-  <!DOCTYPE html>
-  <html lang="en">
-    <head>
-      ${head.join('\n')}
-    </head>
-    <body>
-      ${body}
-    </body>
-  </html>
-  `
+  test('should render dangerouslySetInnerHtml without escaping', () => {
+    const code = `<div>should not escape</div>`
+    const App = () => {
+      return (
+        <div>
+          <div>hoge</div>
+          <Img src="some-url" placeholder="placeholder-url" />
+          <div dangerouslySetInnerHTML={{ __html: code }} />
+        </div>
+      )
+    }
 
-  expect(html).toBe(`
-  <!DOCTYPE html>
-  <html lang="en">
-    <head>
-      <title>some title</title><meta content="Nano-JSX application" name="description" />
-    </head>
-    <body>
-      <div><img src="placeholder-url" /></div>
-    </body>
-  </html>
-  `)
-  expect(spy).not.toHaveBeenCalled()
-})
+    const app = Nano.renderSSR(<App />)
+    const { body } = Helmet.SSR(app)
 
-test("should escape attribute's string value", () => {
-  const content = Nano.h('div', { id: '"hoge' }, 'hoge')
-  const html = renderSSR(content)
-  expect(html).toBe('<div id="&quot;hoge">hoge</div>')
-})
-
-test('should escape text node', () => {
-  const content = Nano.h('div', {}, '<span>span</span>')
-  const html = renderSSR(content)
-  expect(html).toBe('<div>&lt;span&gt;span&lt;/span&gt;</div>')
-})
-
-test('should render dangerouslySetInnerHtml without escaping', () => {
-  const code = `<div>should not escape</div>`
-  const App = () => {
-    return (
-      <div>
-        <div>hoge</div>
-        <Img src="some-url" placeholder="placeholder-url" />
-        <div dangerouslySetInnerHTML={{ __html: code }} />
-      </div>
-    )
-  }
-
-  const app = Nano.renderSSR(<App />)
-  const { body } = Helmet.SSR(app)
-
-  expect(body).toBe(`<div><div>hoge</div><img src="placeholder-url" /><div><div>should not escape</div></div></div>`)
-  expect(spy).not.toHaveBeenCalled()
+    expect(body).toBe(`<div><div>hoge</div><img src="placeholder-url" /><div><div>should not escape</div></div></div>`)
+    expect(spy).not.toHaveBeenCalled()
+  })
 })
