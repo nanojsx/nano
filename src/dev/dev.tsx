f9bcadf887717253b4ebe8e3469c1a48584b1a02
yandeu <20306025+yandeu@users.noreply.github.com>
2022-01-14 00:05:47 +0100

    dev

--- src/dev/dev.tsx
+++ src/dev/dev.tsx
@@ -1,9 +1,73 @@
 import { h, render } from '../core'
 import { Component } from '../component'
+import { useState } from '../hooks'
+
+const ClickMe = () => {
+  const id = 'some-id'
+  const [counter, setCounter, getState, counterEffect] = useState(1, id)
+
+  let output: HTMLParagraphElement
+
+  counterEffect(state => {
+    output.innerText = state.toString()
+  })
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setCounter(getState(id) + 1)
+        }}
+      >
+        click me
+      </button>
+      <p
+        ref={(el: HTMLParagraphElement) => {
+          output = el
+        }}
+      >
+        {counter}
+      </p>
+    </div>
+  )
+}
+
+class Clicks extends Component {
+  constructor(props: any) {
+    super(props)
+
+    this.initState = { counter: 1 }
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={() => this.setState({ ...this.state, counter: this.state.counter + 1 }, true)}>
+          click me
+        </button>
+        <p>{this.state.counter}</p>
+      </div>
+    )
+  }
+}
 
 class App extends Component {
+  public didMount() {
+    setInterval(() => {
+      this.update()
+    }, 2000)
+  }
+
   render() {
-    return <div>Nano JSX App</div>
+    return (
+      <div style="padding: 16px;">
+        <h3>Class Component</h3>
+        <Clicks />
+        <hr />
+        <h3>Functional Component</h3>
+        <ClickMe />
+      </div>
+    )
   }
 }
 
--- src/hooks/useState.ts
+++ src/hooks/useState.ts
@@ -1,18 +1,32 @@
 import { _state } from '../state'
 
-export const useState = <T>(state: T, id: string): readonly [T, (state: T) => void] => {
+export const useState = <T>(
+  state: T,
+  id: string
+): readonly [T, (state: T) => void, (id: string) => T, (cb: (state: T) => void) => void] => {
+  let c: (state: T) => void = _ => {}
+
   const s = {
-    setState(state: T) {
-      if (state !== null) _state.set(id, state)
-    },
     get state(): T {
       return _state.get(id)
+    },
+    setState(state: T) {
+      if (state !== null) {
+        _state.set(id, state)
+        c(state)
+      }
+    },
+    getState(id: string): T {
+      return _state.get(id)
+    },
+    listener(cb: (state: T) => void) {
+      c = cb
     }
   }
 
   if (!_state.has(id)) _state.set(id, state)
 
-  return [s.state, s.setState]
+  return [s.state, s.setState, s.getState, s.listener]
 }
 
 export const getState = (id: string) => {
