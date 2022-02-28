30502ac25413844abe3e452166e26e24144960ab
yandeu <20306025+yandeu@users.noreply.github.com>
2022-02-28 16:59:28 +0100

    v0.0.30

--- deno_lib/core.ts
+++ deno_lib/core.ts
@@ -226,7 +226,7 @@ export const h = (tagNameOrComponent: any, props: any = {}, ...children: any[]) 
     if (0 !== p.indexOf('on')) return false
 
     // we return true if SSR, since otherwise it will get rendered
-    if (el.ssr) return true
+    if (el._ssr) return true
 
     // check if the event is present in the element as object (null) or as function
     return typeof el[p] === 'object' || typeof el[p] === 'function'
