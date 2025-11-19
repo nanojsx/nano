c1a388e9d60f5d580af9c05167eccdecfbb75044
yandeu <20306025+yandeu@users.noreply.github.com>
2025-11-19 11:14:36 +0100

    Fix: style object to style string (fixes #156)

--- test/nodejs/style.test.tsx
+++ test/nodejs/style.test.tsx
@@ -15,3 +15,16 @@ test('should render without errors', async () => {
   expect(res.outerHTML).toBe('<p style="color:red;font-size:22;">hello world</p>')
   expect(spy).not.toHaveBeenCalled()
 })
+
+test('should only transform the keys, not the values (see https://github.com/nanojsx/nano/issues/156)', async () => {
+  class Test extends Component {
+    render() {
+      return <p style={{ colorOne: '#abcdef', colorTwo: '#ABCDEF' }}>hello world</p>
+    }
+  }
+  const res = Nano.render(<Test />)
+
+  await wait()
+  expect(res.outerHTML).toBe('<p style="color-one:#abcdef;color-two:#ABCDEF;">hello world</p>')
+  expect(spy).not.toHaveBeenCalled()
+})
